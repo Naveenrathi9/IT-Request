1a8343a8441a922ea3ed9aa3dba130e7c126958c
Naveen Rathi <102209027+Naveenrathi9@users.noreply.github.com>
2025-04-19 11:04:24 +0530

    Update sendMail.js

--- it-request-system/backend/utils/sendMail.js
+++ it-request-system/backend/utils/sendMail.js
@@ -2,10 +2,15 @@ const nodemailer = require('nodemailer');
 require('dotenv').config({ path: './backend/.env' });
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // Use SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // important for Render
   },
 });
 
